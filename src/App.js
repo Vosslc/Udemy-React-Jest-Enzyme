504b441a760d1273b514f4b598b5b4f80cbb1ec3
Roundy <nroundy801@gmail.com>
2020-03-03 12:01:31 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +18,11 @@ function App() {
         >
           Learn React
         </a>
-        <p>hello world</p>
-      </header>
+        <p>hello world!!</p>
+        <p>{hello world!!}</p>
+      </header> */}
+      <h1>hello world</h1>
+      {/* <h1>{hello world}</h1> */}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+// import { render } from "@testing-library/react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from "./App";
 
-test('renders hello world', () => {
-  const { getByText } = render(<App />);
-  const helloWorldElent = getByText(/hello world/i);
-  expect(helloWorldElent).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+// test("renders learn react link", () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// test("renders hello world", () => {
+//   const { getByText } = render(<App />);
+//   const helloWorldElent = getByText(/hello world/i);
+//   expect(helloWorldElent).toBeInTheDocument();
+// });
+
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  // console.log(wrapper.debug());
+  // expect(wrapper).toBeTruthy();
+  // expect(wrapper).toBeFalsy();
 });
